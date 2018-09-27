2e9e6f03d8d43c6ceb47bfe0e7443f6f419f7b96
Natalia Go <officiallynatalia@gmail.com>
2018-09-27 16:59:17 -0500

    Initial commit

---
+++ assets/js/nataliago.js
@@ -0,0 +1,15 @@
+UIkit.navbar(element, options);
+UIkit.parallax(element, options);
+
+
+
+
+
+
+// To apply lightbox to a group of links
+UIkit.lightbox(element, options);
+
+// To dynamically initialize the lightbox panel
+UIkit.lightboxPanel(panelOptions);
+
+UIkit.navbar(element, options);
